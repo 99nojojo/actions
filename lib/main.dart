ef5f791e6f8a0956a48c99699be61bc5f2adc34d
Benny <Wlj4572benny@gmail.com>
2024-08-31 20:11:36 +0800

    asd

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_actions/framework/app_widget_bind_observer.dart';
 import 'package:flutter_actions/framework/framework.dart';
 import 'package:flutter_actions/routes/route_manager.dart';
 import 'package:get/get.dart';
-import 'package:get/get_navigation/get_navigation.dart';
-import 'package:get/get_state_manager/get_state_manager.dart';
 import 'package:msh_checkbox/msh_checkbox.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  WidgetsBinding.instance.addObserver(AppObserver());
-  await DioManager.shared.dioInterceptorConfig();
-  await SystemChrome.setPreferredOrientations(
-      [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp]);
+  // WidgetsFlutterBinding.ensureInitialized();
+  // WidgetsBinding.instance.addObserver(AppObserver());
+  // await DioManager.shared.dioInterceptorConfig();
+  // await SystemChrome.setPreferredOrientations(
+  //     [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp]);
   runApp(const MyApp());
 }
 
@@ -45,45 +41,3 @@ class MyApp extends StatelessWidget   {
   }
 }
 
-class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
-
-  @override
-  State<LoginPage> createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
-  @override
-  Widget build(BuildContext context) {
-    Get.put(LoginController());
-    return GetX<LoginController>(builder: (_) {
-      return Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            "android风格",
-            style: TextStyle(color: Colors.black),
-          ),
-        ),
-        body: Center(
-          child: MSHCheckbox(
-            size: 32,
-            style: MSHCheckboxStyle.fillFade,
-            value: _.isCheck.value,
-            colorConfig: MSHColorConfig(checkColor: (state) {
-              return Colors.amber;
-            }, borderColor: (state) {
-              return state.isDisabled ? Colors.blue : Colors.deepPurple;
-            }),
-            onChanged: (selected) {
-              _.isCheck.value = selected;
-            },
-          ),
-        ),
-      );
-    });
-  }
-}
-
-class LoginController extends GetxController {
-  var isCheck = false.obs;
-}
